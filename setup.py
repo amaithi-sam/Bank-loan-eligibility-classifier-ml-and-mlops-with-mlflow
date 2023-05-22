e7063bed760eb650ebf503b4e09ed2ec3ca1127b
Amaithi <amaithichirasan.s@gmail.com>
2023-05-22 20:31:17 +0530

    Initial Project Setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name='Banking_prediction',
+    version='0.0.1',
+    description="it's a MlOps Package of Bank dataset",
+    author='Amaithi Chirasan S'
+    packages=find_packages(),
+    license='MIT'
+)
