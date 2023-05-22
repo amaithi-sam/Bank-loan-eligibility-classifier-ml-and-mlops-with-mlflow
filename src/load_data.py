149e8c492757935d0dc5c6019105c65a5dd2426f
Amaithi <amaithichirasan.s@gmail.com>
2023-05-22 20:48:22 +0530

    create src structure

---
+++ src/load_data.py
@@ -0,0 +1,8 @@
+import os 
+from get_data_params import read_params, get_data 
+import argparse 
+
+def load_and_save(config_path):
+    config = read_params(config_path)
+    df = get_data(config_path)
+    
