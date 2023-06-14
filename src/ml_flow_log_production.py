f80061c561f629f2e74eac43bc006f2689355263
Amaithi <amaithichirasan.s@gmail.com>
2023-06-14 09:02:40 +0530

    finalizing the dvc pipeline

--- src/ml_flow_log_production.py
+++ src/ml_flow_log_production.py
@@ -17,19 +17,21 @@ def log_production_model(config_path):
 
     mlflow.set_tracking_uri(remote_server_uri)
 
-    runs = mlflow.search_runs(experiment_ids=["1"]) # it will return a dataframe
-    lowest = runs["metrics.mae"].sort_values(ascending=True)[0]
-    lowest_run_id = runs[runs["metrics.mae"] == lowest]["run_id"][0]
+    runs = mlflow.search_runs(search_all_experiments=True) # it will return a dataframe
+    # runs.to_csv('runs.csv')
+    lowest = runs["metrics.accuracy"].sort_values(ascending=True)[0]
+    lowest_run_id = runs[runs["metrics.accuracy"] == lowest]["run_id"][0]
 
     client = MlflowClient()
     
     for mv in client.search_model_versions(f"name='{model_name}'"):
         mv = dict(mv)
+        # pprint(mv)
 
         if mv["run_id"] == lowest_run_id:
             current_version = mv["version"]
             logged_model = mv["source"]
-            pprint(mv, indent=4)
+            # pprint(mv, indent=4)
             client.transition_model_version_stage(
                 name=model_name,
                 version=current_version,
@@ -43,8 +45,7 @@ def log_production_model(config_path):
                 stage="Staging",
                     )
     loaded_model =mlflow.pyfunc.load_model(logged_model)
-    model_path = config["webapp_model_dir"]
-
+    model_path = mlflow_config['production_model_path']
     joblib.dump(loaded_model, model_path)
     
 
@@ -53,5 +54,5 @@ if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
-    data = log_production_model(config_path=parsed_args.config)
+    log_production_model(config_path=parsed_args.config)
 
